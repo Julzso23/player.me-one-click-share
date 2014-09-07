a549dfe251ed3b784bbdbdfbc4797d1a4882c26a
Julzso23 <julian@julxzs.eu>
2014-09-07 21:15:36 +0100

    Added posting from the context menu

---
+++ js/background.js
@@ -0,0 +1,22 @@
+chrome.contextMenus.create(
+	{
+		"type": "normal",
+		"title": "Share on player.me",
+		"onclick": function(info, tab)
+		{
+			$.ajax({
+				type: "POST",
+				url: "https://player.me/api/v1/feed",
+				data: {"post": "[&nbsp;]("+tab.url+")"},
+				dataType: "json"
+			}).done(function(data)
+			{
+				if(data.results == "Must be logged on to do this")
+				{
+					chrome.notifications.create("", {"type": "basic", "iconUrl": "icon.png", "title": "ERROR", "message": "You must be logged into player.me to share."}, function(id){});
+					chrome.tabs.create({"url": "https://player.me"});
+				}
+			});
+		}
+	}
+);
