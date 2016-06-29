a01e31b9e1d42b8f6341a815ca54bdc010b1972f
Julian <julzso23@gmail.com>
2016-06-30 00:21:07 +0100

    Added App component.

---
+++ components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Post from './Post';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Post />
+        );
+    }
+}
