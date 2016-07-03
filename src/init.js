caa092e0065c0b890e245edcc7a8d38425ca5cb4
Jamie Simmonds <jamie.simmonds96@gmail.com>
2016-07-03 12:46:17 +0100

    Include static assets in init.js

--- src/init.js
+++ src/init.js
@@ -3,4 +3,8 @@ import {render} from 'react-dom';
 import App from './components/App';
 import './styles/body.css';
 
+import './images/logo-icon-128.png';
+import './images/logo-icon-16.png';
+import './manifest.json';
+
 render(<App />, document.getElementById('app'));
