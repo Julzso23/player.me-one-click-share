325a2a8da2ad6ec861087e5ba4a72b276497c068
Julian <julzso23@gmail.com>
2016-07-06 22:57:29 +0100

    Added game check-in as a seperate page.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
 import Post from './Post';
 import Login from './Login';
+import Layout from './Layout';
+import GameQuery from './GameQuery';
 import Spinner from './Spinner';
 import request from 'superagent';
 
@@ -21,9 +23,12 @@ export default class App extends React.Component {
     render() {
         return (
             <Router history={browserHistory}>
-                <Route path='post' component={Post}></Route>
-                <Route path='login' component={Login}></Route>
-                <Route path='*' component={Spinner}></Route>
+                <Route path='/' component={Layout}>
+                    <Route path='post' component={Post}></Route>
+                    <Route path='login' component={Login}></Route>
+                    <Route path='check-in' component={GameQuery}></Route>
+                    <Route path='*' component={Spinner}></Route>
+                </Route>
             </Router>
         );
     }
