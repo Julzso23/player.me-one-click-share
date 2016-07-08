9a26b6c509419f91726a4b31c93931540ddd33f0
Julian <julzso23@gmail.com>
2016-07-08 18:00:41 +0100

    Call onSelect event when a game is clicked.

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -11,9 +11,13 @@ export default class GameList extends React.Component {
     componentWillReceiveProps(nextProps) {
         let games = [];
         nextProps.games.forEach(game => {
-            games.push(<li key={game.id}>{game.title}</li>);
+            games.push(<li key={game.id} data-id={game.id} onClick={this.handleClick.bind(this)}>{game.title}</li>);
         });
         this.setState({games: games});
+    }
+
+    handleClick(event) {
+        this.props.onSelect(this.props.games.find(game => game.id === Number(event.target.dataset.id)));
     }
 
     render() {
