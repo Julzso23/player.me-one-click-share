3d2a9ab1080c5c518d5206c6855713c6077f8371
Julian <julzso23@gmail.com>
2016-07-09 18:35:18 +0100

    Config refactoring.

--- webpack.config.js
+++ webpack.config.js
@@ -14,34 +14,35 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel',
-                query: { presets: [ 'es2015', 'react' ] }
+                query: {
+                    presets: ['es2015', 'react']
+                }
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                loaders: ['style', 'css']
             },
             {
-                test: /\.(png|json)$/,
-                loader: 'file-loader',
+                test: /\.json$/,
+                loader: 'file',
                 query: {
                     name: '[name].[ext]'
                 }
             },
             {
-                test   : /\.woff/,
-                loader : 'url?prefix=font/&limit=10000&mimetype=application/font-woff'
+                test: /\.png$/,
+                loader: 'file',
+                query: {
+                    name: 'images/[name].[ext]'
+                }
             },
             {
-                test   : /\.ttf/,
-                loader : 'file?prefix=font/'
-            },
-            {
-                test   : /\.eot/,
-                loader : 'file?prefix=font/'
-            },
-            {
-                test   : /\.svg/,
-                loader : 'file?prefix=font/'
+                test: /\.(ttf|eot|svg|woff)/,
+                loader: 'file',
+                query: {
+                    prefix: 'font/',
+                    name: 'fonts/[name].[ext]'
+                }
             }
         ]
     },
